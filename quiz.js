3c0f0d796257c5930f085e6bbbce9d71bfbec3b0
cleriphil <cleri.phil@gmail.com>
2016-04-13 20:04:11 -0700

    add event listener to check change artQuiz.currentQuestion - incomplete

--- quiz.js
+++ quiz.js
@@ -1,25 +1,24 @@
 
-function Quiz(questions) {
-  this.questions = questions;
-  this.currentQuestion = questions[0];
-  this.score = 0;
-}
-
-/*Quiz.prototype.nextQuestion = function(){
-  var current = this.currentQuestion;
-  var currentIndex = this.questions.indexOf(current);
-  this.currentQuestion = this.questions[currentIndex + 1]
-}*/
-
-
 /*Quiz.prototype.checkAnswer = function(){
   if(document.getElementById(this.currentQuestion.correctAnswer).checked) {
     this.score ++;
   }
 }*/
 
-
 window.onload = function() {
+
+  function Quiz(questions) {
+    this.questions = questions;
+    this.currentQuestion = questions[0];
+    this.score = 0;
+  }
+
+  Quiz.prototype.nextQuestion = function(){
+    var current = this.currentQuestion;
+    var currentIndex = this.questions.indexOf(current);
+    this.currentQuestion = this.questions[currentIndex + 1];
+    return this.currentQuestion;
+  }
 
   var allQuestions = [{statement: "Who is often given credit for inventing cubism?", choices: ["Georges Braque", "Frida Kahlo", "Francis Picabia", "Marcel Duchamp"], correctAnswer:0},{statement: "Matisse died in 1954 leaving his ceiling covered with drawings. How come?", choices: ["After 60 years of paint fumes he wasn\'t \'all there\' towards the end.","He was doing preliminary sketches for a ceiling fresco.","He was bedridden and drew using a long stick.","He ran out of walls to draw on."], correctAnswer:2}];
   artQuiz = new Quiz(allQuestions);
@@ -39,7 +38,13 @@ window.onload = function() {
     choicesUI.appendChild(choiceLi);
     choiceLi.innerHTML = "<input type=\'radio\' id=\'" + i + "\'name=\'action\' value=\'" + i + "\'>" + choice + "<br/>";
   }
+
+  var nextButton = document.getElementById('nextBtn');
+  nextButton.addEventListener('click', function() { artQuiz.nextQuestion() }, false);
+  //nextButton.addEventListener('click', function() { artQuiz.nextQuestion.bind(artQuiz) }, false);
 };
 
 //After submitting answer
 //artQuiz.checkAnswer();
+
+//http://stackoverflow.com/questions/14285748/using-addeventlistener-on-prototype
